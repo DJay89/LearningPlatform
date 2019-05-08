8f0e6550ed59317d1832f69016398a4256b778a1
Danny Jønsson <dannyjoensson@gmail.com>
2019-05-08 18:51:51 +0200

    Added file validation to fileuploadmodel

--- LearningPlatform/Models/FileUploadModel.cs
+++ LearningPlatform/Models/FileUploadModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LearningPlatform.Models.Validators;
+using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -6,15 +7,24 @@ namespace LearningPlatform.Models
 {
     public class FileUploadModel
     {
-        [Required]
+        [Required(ErrorMessage = "You must provide a file")]
         public IFormFile File { get; set; }
 
         [Required]
+        [Range(1, int.MaxValue - 1, ErrorMessage = "First page must be equal or greater than 1")]
         public int StartPage { get; set; }
 
         [Required]
+        [PageValidate]
         public int EndPage { get; set; }
            
         public List<PageModel> Pages { get; set; }
+
+        public FileUploadModel()
+        {
+            this.StartPage = 1;
+            this.EndPage = 2;
+            this.Pages = new List<PageModel>();
+        }
     }
 }
---
+++ LearningPlatform/Models/Validators/PageValidate.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace LearningPlatform.Models.Validators
+{
+    public class PageValidate : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            FileUploadModel fileUploadModel = (FileUploadModel) validationContext.ObjectInstance;
+           
+            if ((int) value <= fileUploadModel.StartPage)
+            {
+                return new ValidationResult("The end page must be higher than start page");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
