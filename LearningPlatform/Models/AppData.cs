27e27c2cdafcd05a2447996966224e39bdc3f59c
Danny Jønsson <dannyjoensson@gmail.com>
2019-05-08 12:25:44 +0200

    created models for the data and implemented an api to retrieve the data. Also created seeders for some test data. Removed school context as it was a test structure

---
+++ LearningPlatform/Models/AppData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningPlatform.Models
+{
+    public class AppData
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public bool IsTextUnderstood { get; set; }
+        public int WPM { get; set; }
+        public int WordCount { get; set; }
+        public string Mode { get; set; }
+
+        public User User { get; set; }
+    }
+}
